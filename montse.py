15eb59fda7c5450bc58f347316a2581ec4dd7045
DaniCassani <danielduran1995@gmail.com>
2021-09-24 21:41:50 +0200

    yes

--- montse.py
+++ montse.py
@@ -438,9 +438,9 @@ async def _impuestos(ctx):
         respuesta += ":chart_with_downwards_trend: **{}** ha recibido un gravamen del **85%** ({} lexos).\n".format(usuario,impuesto)
     if len(respuesta)>0:
       respuesta_embed = discord.Embed(title="__**IMPUESTOS A LAS GRANDES FORTUNAS:**__" , color= 0xff0000)
-      respuesta ="\n" + respuesta + "👑 SU MAJESTAD 👑 lo gestionará con sabiduría. :crown: :blush:"
+      respuesta += "👑 SU MAJESTAD 👑 lo gestionará con sabiduría. :crown: :blush:"
       respuesta_embed.add_field(name="Gravámenes",value = respuesta)
-      await ctx.reply(embed = respuesta)
+      await ctx.reply(embed = respuesta_embed)
     #actjson.actualizar_lexos(lexos)
 
 
