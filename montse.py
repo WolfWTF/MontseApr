3577645e38b3baf9fde8c9e8f2c87dc0e2140513
DaniCassani <danielduran1995@gmail.com>
2021-09-24 21:53:13 +0200

    si

--- montse.py
+++ montse.py
@@ -437,8 +437,8 @@ async def _impuestos(ctx):
         lexos["Alexander Alex"]["lexos"] = arcas_reales
         respuesta += ":chart_with_downwards_trend: **{}** ha recibido un gravamen del **85%** ({} lexos).\n".format(usuario,impuesto)
     if len(respuesta)>0:
-      respuesta_embed = discord.Embed(title="__**IMPUESTOS A LAS GRANDES FORTUNAS:**__" , color= 0xff0000)
-      respuesta_embed.add_field(name="",value = respuesta)
+      respuesta_embed = discord.Embed(title="__**IMPUESTOS A LAS GRANDES FORTUNAS:**__" , color= 0xff0000, description = respuesta)
+      #respuesta_embed.add_field(name="",)
       footer = "👑 SU MAJESTAD 👑 lo gestionará con sabiduría. 👑😊"
       respuesta_embed.set_footer(text = footer)
       await ctx.reply(embed = respuesta_embed)
